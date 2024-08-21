c922f80a3fd7c1d17568bf529868d2b47330e792
unknown <dwlee0802@gmail.com>
2024-08-21 22:39:03 +0900

    Spaceship Movement Control.

--- Scenes/spaceship.gd
+++ Scenes/spaceship.gd
@@ -1,15 +1,29 @@
 extends RigidBody2D
 class_name Spaceship
 
+## Physics Parameters
 @export
 var speed_max: float = 300
 var speed: float = 0
 var acceleration: float = 10
+@export
+var thrust_force: float = 100
+@export
+var after_burner_multiplier: float = 3
 
 @export
 var angular_max: float = 10
 var angular_acceleration: float = 5
 
+## Ship Body
+@onready
+var ship_body: Node2D = $ShipBody
+@onready
+var flame_sprite: Sprite2D = $ShipBody/FlameSprite
+@onready
+var afterburner_sprite: Sprite2D = $ShipBody/AfterburnerSprite
+@onready
+var flame_particles: CPUParticles2D = $ShipBody/FlameParticles
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -18,16 +32,24 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	flame_sprite.visible = false
+	flame_particles.emitting = false
+	afterburner_sprite.visible = false
 	if Input.is_action_pressed("forward"):
-		speed += delta * acceleration
-		if speed > speed_max:
-			speed = speed_max
-	linear_velocity = Vector2.from_angle(rotation) * speed
+		var dir: Vector2 = Vector2.from_angle(ship_body.rotation)
+		if Input.is_action_pressed("after_burner"):
+			apply_central_impulse(dir * thrust_force * after_burner_multiplier)
+			afterburner_sprite.visible = true
+		else:
+			apply_central_impulse(dir * thrust_force)
+			
+		flame_sprite.visible = true
+		flame_particles.emitting = true
 	
+	# rotate sprite
+	# make this physics based?
 	if Input.is_action_pressed("rotate_right"):
-		if angular_velocity < angular_max:
-			apply_torque_impulse(angular_acceleration * delta)
+		ship_body.rotate(angular_acceleration * delta)
 	if Input.is_action_pressed("rotate_left"):
-		if angular_velocity > -angular_max:
-			apply_torque_impulse(-angular_acceleration * delta)
-		
+		ship_body.rotate(-angular_acceleration * delta)
+			
---
+++ Scenes/spaceship_game.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+@onready
+var ship_info_label: Label = $CanvasLayer/ShipInfoLabel
+@onready
+var player_spaceship : Spaceship = $Spaceship
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	ship_info_label.text = ""
+	ship_info_label.text += "LINEAR SPD: " + str(player_spaceship.linear_velocity.length()) + "\n"
+	ship_info_label.text += "ANGULAR SPD: " + str(player_spaceship.angular_velocity)
