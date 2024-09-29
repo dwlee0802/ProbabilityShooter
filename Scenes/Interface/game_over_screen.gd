e97b6cc70e93d02194ff7c7bc83b2050aa2e0a08
unknown <dwlee0802@gmail.com>
2024-09-29 23:34:19 +0900

    Added labels to damage output.

--- Scenes/Interface/game_over_screen.gd
+++ Scenes/Interface/game_over_screen.gd
@@ -15,8 +15,6 @@ var kill_count_label: Label = $Control/vBoxContainer/StatsContainer/StatLabels/K
 var level_label: Label = $Control/vBoxContainer/StatsContainer/StatLabels/Level
 @onready
 var damage_output: Label = $Control/vBoxContainer/StatsContainer/StatLabels/DamageOutput
-@onready
-var effective_damage: Label = $Control/vBoxContainer/StatsContainer/StatLabels/EffectiveDamage
 
 
 func _ready() -> void:
@@ -33,4 +31,4 @@ func set_game_over_stats(stats_component: StatisticsComponent) -> void:
 	survival_time_label.text = str(int(stats_component.survival_time)) + "s"
 	kill_count_label.text = str(int(stats_component.kill_count)) + " kills"
 	level_label.text = "Lv. " + str(stats_component.level_reached) + " (" + str(stats_component.total_exp_gained) + " EXP)"
-	damage_output.text = str(stats_component.total_effective_damage_output) + " / " + str(stats_component.total_damage_output)
+	damage_output.text = "Effective: " + str(stats_component.total_effective_damage_output) + " / Total: " + str(stats_component.total_damage_output)
