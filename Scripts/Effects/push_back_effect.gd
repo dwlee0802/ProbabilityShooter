67cea92fae35d27b204ec0716880879d61fc0146
unknown <dwlee0802@gmail.com>
2024-10-12 16:58:58 +0900

    Added strength field to push back.

--- Scripts/Effects/push_back_effect.gd
+++ Scripts/Effects/push_back_effect.gd
@@ -1,6 +1,9 @@
 extends Effect
 class_name PushBackEffect
 
+@export
+var strength: float = 1000
+
 func activate(_game_ref, event: Event):
 	if event.subject is PlayerUnit:
 		event.subject.push_back()
