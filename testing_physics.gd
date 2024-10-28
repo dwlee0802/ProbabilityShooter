8530c32ce1639f7f00633cc36ed33eb65d539c9e
unknown <dwlee0802@gmail.com>
2024-10-28 22:53:29 +0900

    Organized files.

--- testing_physics.gd
+++
@@ -1,6 +0,0 @@
-extends RigidBody2D
-
-var movement_component: WASDMovementComponent = WASDMovementComponent.new()
-
-func _physics_process(delta: float) -> void:
-	movement_component.physics_update(self, delta)
