6d9980f5e05292c7de807ad8d855ebd77a7e31b0
unknown <dwlee0802@gmail.com>
2024-11-01 20:08:43 +0900

    Changed score label to use tween.

--- Scenes/Interface/score_info.gd
+++ Scenes/Interface/score_info.gd
@@ -7,6 +7,8 @@ var score_label: Label = $ScoreLabel
 var displayed_score: float = 0
 var displayed_score_increase: float = 500
 
+var score_tween: Tween = null
+
 @onready
 var multiplier_label: Label = $MultiplierLabel
 @onready
@@ -18,7 +20,15 @@ func set_score_component(component: ScoreComponent):
 	_score_component.multiplier_changed.connect(on_multiplier_changed)
 	
 func on_score_changed():
-	score_label.text = "SCORE: " + str(_score_component.total_score)
+	if score_tween:
+		score_tween.kill()
+		score_tween = null
+	var new_tween: Tween = create_tween()
+	score_tween = new_tween
+	
+	score_tween.tween_property(self, "displayed_score", _score_component.total_score, 0.2)
+	
+	score_label.text = "SCORE: " + str(displayed_score)
 
 func on_multiplier_changed():
 	multiplier_label.text = "BONUS: " + str(int(_score_component.get_multiplier_bonus() * 100)) + "%"
@@ -28,11 +38,6 @@ func _process(_delta: float) -> void:
 		multiplier_decay_bar.value = (_score_component.decay_timer.time_left / _score_component.decay_timer.wait_time) * 100.0
 	else:
 		multiplier_decay_bar.value = 0
-	
-	if _score_component.total_score > displayed_score:
-		displayed_score += displayed_score_increase * _delta
-	else:
-		displayed_score = _score_component.total_score
 		
-	score_label.text = "SCORE: " + str(int(displayed_score))
-	
+	if score_tween != null and score_tween.is_valid():
+		score_label.text = "SCORE: " + str(int(displayed_score))
--- Scenes/score_component.gd
+++ Scenes/score_component.gd
@@ -8,24 +8,29 @@ var kill_score_amount: int = 100
 
 @export_category("Multiplier")
 @export
-var multiplier_level: int = 0
+var multiplier_bonus: float = 0
 @export
-var multiplier_per_level: float = 0.25
+var bonus_per_kill: float = 20
 @export
 var multiplier_decay_time: float = 1
 var decay_timer: Timer
 
 signal score_changed
-signal multiplier_changed
 
 
 func _ready() -> void:
 	decay_timer = Timer.new()
 	add_child(decay_timer)
-	decay_timer.autostart = false
+	decay_timer.autostart = true
 	decay_timer.one_shot = false
 	decay_timer.timeout.connect(on_decay_timeout)
-	
+
+func _process(delta: float) -> void:
+	if multiplier_bonus > 0:
+		multiplier_bonus -= delta
+	else:
+		multiplier_bonus = 0
+		
 # add score and return added amount
 func on_kill() -> int:
 	# add score
@@ -34,8 +39,7 @@ func on_kill() -> int:
 	score_changed.emit()
 	
 	# increase multiplier
-	multiplier_level += 1
-	multiplier_changed.emit()
+	multiplier_bonus += bonus_per_kill
 	
 	# reset multiplier decay timer
 	reset_decay()
@@ -43,20 +47,13 @@ func on_kill() -> int:
 	return amount
 
 func get_multiplier_bonus() -> float:
-	return multiplier_level * multiplier_per_level
-
-func on_decay_timeout() -> void:
-	multiplier_level = 0
-	multiplier_changed.emit()
-	if multiplier_level == 0:
-		decay_timer.stop()
+	return int(multiplier_bonus) * 0.01
 
 func reset_decay() -> void:
 	decay_timer.stop()
 	decay_timer.start(multiplier_decay_time)
 	
 func reset() -> void:
-	multiplier_level = 0
-	multiplier_changed.emit()
+	multiplier_bonus = 0
 	total_score = 0
 	score_changed.emit()
