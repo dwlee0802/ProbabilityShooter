174d91589186e463f72e2f0801d5141491af3fe8
unknown <dwlee0802@gmail.com>
2024-11-16 20:19:37 +0900

    Reorganizing weapon code.

--- Scenes/Units/player_weapon.gd
+++ Scenes/Units/player_weapon.gd
@@ -119,7 +119,7 @@ func _unhandled_input(event: InputEvent) -> void:
 
 func _process(delta: float) -> void:
 	state_machine.process_frame(delta)
-	
+
 func update_aim_cone() -> void:
 	var spread: float = weapon_data.get_spread_in_rad()
 	aim_cone.polygon = cone_from_angle(spread, 100000)
@@ -167,7 +167,38 @@ func get_magazine_status() -> String:
 	output += " / " + str(magazine_size)
 	
 	return output
-
+	
+func on_activation(unit: Unit, mouse_position: Vector2):
+	var current_bullet: Bullet = bullets.pop_front()
+	
+	for i in range(current_bullet.projectile_count):
+		# make new projectile
+		var new_bullet: Projectile = projectile_scene.instantiate()
+			
+		var random_spread_offset: float = randf_range(-get_spread()/2, get_spread()/2)
+		# set stats
+		# save origin unit to call back for experience gain
+		new_bullet.origin_unit = unit
+		new_bullet.launch(
+			mouse_position.normalized().rotated(random_spread_offset * (current_bullet.projectile_count)), 
+			get_projectile_speed(), 
+			int(current_bullet.damage_amount / float(current_bullet.projectile_count)), 
+			data.knock_back_force)
+		new_bullet.global_position = bullet_spawn_position.global_position
+		
+		new_bullet.bullet_data = current_bullet
+		if current_bullet.piercing:
+			new_bullet.pierce_count += 1
+		
+		# add to scene
+		unit.get_tree().root.get_node("Game").projectiles.add_child(new_bullet)
+	
+	current_magazine_count -= 1
+	
+	CameraControl.camera.shake_screen(20,200)
+	
+	super.on_activation(unit, mouse_position)
+	
 func point_arm_at(target_pos: Vector2) -> void:
 	var angle: float = Vector2.RIGHT.angle_to_point(target_pos)
 	arm.rotation = angle
